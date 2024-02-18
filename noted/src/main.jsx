f385f883cb2946aa4dbc44fe53664f74955cf6ae
yurikahirata <yurikahirata0130@gmail.com>
2024-02-18 22:38:13 +0900

    feat: react app with root page

---
+++ noted/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Root from './pages/root.jsx';
+import App from './App.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ noted/src/pages/root.jsx
@@ -0,0 +1,15 @@
+import '../styles/root.css';
+
+export default function Root() {
+  return (
+    <>
+      <section className="section-content">
+        <h1>n o t e d .</h1>
+        <div>
+          <button className="root-btn">login</button>
+          <button className="root-btn">sign-up</button>
+        </div>
+      </section>
+    </>
+  )
+}
